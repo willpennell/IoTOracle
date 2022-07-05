ec5cdd1ffbe09ee55003c2769438fc52effd4add
willp <will.phanta@gmail.com>
2022-07-05 18:59:03 +0100

    ABI gen

--- IoToracle/core/eventlistener.go
+++ IoToracle/core/eventlistener.go
@@ -1,20 +1,38 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
 )
 
 func main() {
-	client, err := ethclient.Dial("http://localhost:8545")
+	client, err := ethclient.Dial("http://localhost:7545")
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("We are connected")
 	_ = client
-	/*contractAddress := common.HexToAddress(" 0x7d79D4256C9Ed0B416250b3d5B6CD5365bc93188")
+	contractAddress := common.HexToAddress("0xa8013c6b98043a53C812Ee6C042ff860F4ebb14e")
 	query := ethereum.FilterQuery{
 		Addresses: []common.Address{contractAddress},
-	}*/
+	}
+	logs := make(chan types.Log)
+	sub, err := client.SubscribeFilterLogs(context.Background(), query, logs)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		select {
+		case err := <-sub.Err():
+			log.Fatal(err)
+		case vLog := <-logs:
+			fmt.Println(vLog)
+		}
+	}
+
 }
