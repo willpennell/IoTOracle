a0336e523a947411d9c423e17efcaa5ebc56b69b
willp <will.phanta@gmail.com>
2022-07-05 18:34:02 +0100

    IoTOracle Tests Passed

--- IoToracle/core/eventlistener.go
+++ IoToracle/core/eventlistener.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	"github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/common"
+	"fmt"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
 )
 
 func main() {
-	client, err := ethclient.Dial("http://localhost:7545")
+	client, err := ethclient.Dial("http://localhost:8545")
 	if err != nil {
 		log.Fatal(err)
 	}
-	contractAddress := common.HexToAddress(" 0x7d79D4256C9Ed0B416250b3d5B6CD5365bc93188")
+	fmt.Println("We are connected")
+	_ = client
+	/*contractAddress := common.HexToAddress(" 0x7d79D4256C9Ed0B416250b3d5B6CD5365bc93188")
 	query := ethereum.FilterQuery{
 		Addresses: []common.Address{contractAddress},
-	}
+	}*/
 }
--- IoToracle/test/testIoTOracleContract.test.js
+++ IoToracle/test/testIoTOracleContract.test.js
@@ -3,12 +3,34 @@ const IoTOracleContract = artifacts.require("IoTOracleContract")
 contract("IoTOracleContract", (accounts) =>{
     let IoTOracleInstance;
     let expectedRequestID = 1;
+    let callbackAddr = accounts[1];
+    let iotId = 'rp42022';
+    let requiredResult = true;
 
     before(async () => {
         IoTOracleInstance = await IoTOracleContract.deployed();
     });
+    it("should make call to IoTOracle.makeRequest() and return ID value: 1", async () => {
 
-    it()
+        const returnedRequestID = await IoTOracleInstance.makeRequest.call(
+            callbackAddr, iotId, requiredResult, {from: callbackAddr}
+        );
+        assert.equal(returnedRequestID, expectedRequestID, "These should be equal");
+    });
+    it("Should emit Event RequestInfo and match expected outputs.", async () => {
+        const event = await IoTOracleInstance.makeRequest.sendTransaction(
+            callbackAddr, iotId, requiredResult, {from: callbackAddr}
+        );
+        assert.equal(event.logs.length, 1, "RequestInfo should have been emitted");
+        assert.equal(event.logs[0].args.requester.valueOf(), callbackAddr);
+        assert.equal(event.logs[0].args.requiredResult.valueOf(), true);
+        assert.equal(event.logs[0].args.id.valueOf(), expectedRequestID);
+    });
+
+
+
+
+
 
 
 });
